721a9736d108d371e8994bdd1f0e43070b710b0d
evermade1 <scmy1013@naver.com>
2023-07-09 22:59:27 +0900

    alg done but why

--- App.js
+++ App.js
@@ -1,38 +1,74 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
-const API_KEY = "RGAPI-36a7789d-c95f-4a3c-9192-c397cbca5437"
-const NICKNAME = "tueresmiflor"
+const API_KEY = "RGAPI-13fde9f9-0098-405b-84a0-4900fef8d128"
 
 export default function App() {
-  const [id,setId] = useState("")
-  const [data, setData] = useState([])
-  const abc = async () => {
-    const response = await fetch(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${NICKNAME}?api_key=${API_KEY}`)
+  const [text, setText] = useState("")
+  const [summonerName, setSummonerName] = useState("")
+  const [summonerData, setSummonerData] = useState([])
+  const [rankData, setRankData] = useState([])
+  const abc = async (name) => {
+    const response = await fetch(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${name}?api_key=${API_KEY}`)
     const json = await response.json()
-    setId(json.id)
-    const response1 = await fetch(`https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}?api_key=${API_KEY}`)
-    const json1 = await response1.json()
-    setData(json1[0])
+    setSummonerData(json)
+    if (json.hasOwnProperty("status")) {
+      return
+      }
+    else{
+      const response1 = await fetch(`https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${json.id}?api_key=${API_KEY}`)
+      const json1 = await response1.json()
+      setRankData(json1[0])
+    }
+    }
+    useEffect(() => {
+      console.log(rankData);
+    }, [rankData]);
+  const sendSummonerName = async () => {
+    
+    setSummonerName(text)
+    await abc(text)
+    setText("")
   }
-  useEffect(() => {
-    abc()
-  }, [])
+  const onChangeText = (name) => {
+    setText(name)
+  }
   return (
     <View style={styles.container}>
-      <Text>{data.summonerName}</Text>
-      <Text>{data.tier} {data.rank}</Text>
+      <TextInput style={styles.button}
+        onSubmitEditing={sendSummonerName}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder='소환사명을 입력하세요.' />
+      {summonerData.hasOwnProperty("status") ? <Text>소환사가 존재하지 않습니다.</Text> :
+        <View style={styles.profile}>
+          <Text>{rankData.summonerName} Lv.{summonerData.summonerLevel}</Text>
+          <Text>{rankData.tier} {rankData.rank} {rankData.leaguePoints}LP</Text>
+        </View>
+      }
+
+
       <StatusBar style="auto" />
     </View>
-  );
-}
+  )
+    }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50
+    // justifyContent: 'center',
   },
+  button: {
+    backgroundColor: "lightgray",
+    padding: 20,
+    borderRadius: 10
+  },
+  profile: {
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
