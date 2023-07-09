7420fe0709bc0e072f5b65b509c94ffd63f9fbaa
Mooyoung Cho <scmy1013@naver.com>
2023-07-09 17:51:33 +0900

    0709-02

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const API_KEY = "RGAPI-36a7789d-c95f-4a3c-9192-c397cbca5437"
+const NICKNAME = "tueresmiflor"
+
 export default function App() {
+  const [id,setId] = useState("")
+  const [data, setData] = useState([])
+  const abc = async () => {
+    const response = await fetch(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/${NICKNAME}?api_key=${API_KEY}`)
+    const json = await response.json()
+    setId(json.id)
+    const response1 = await fetch(`https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${id}?api_key=${API_KEY}`)
+    const json1 = await response1.json()
+    setData(json1[0])
+  }
+  useEffect(() => {
+    abc()
+  }, [])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{data.summonerName}</Text>
+      <Text>{data.tier} {data.rank}</Text>
       <StatusBar style="auto" />
     </View>
   );
